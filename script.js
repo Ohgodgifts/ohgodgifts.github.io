c0c7ae5ed373f6e1d0ba54ed61fef3a2f045726c
Tejana <teja.sanjana@gmail.com>
2023-05-08 13:42:14 -0700

    girlfriend gifts addition

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ function addScenarios() {
 	maindiv.appendChild(renderCards([
 		["Co-worker with new born", "imgs/parentssm.jpg", "coworker-newborn.html"],
 		["Grand kids", "imgs/grandkidsm.jpg", "grandkids.html"],
-	  ["Mom", "imgs/momsm.jpg", "mom.html"]]));
+	  ["Mom", "imgs/momsm.jpg", "mom.html"],
+	  ["Girlfriend / Wife", "imgs/girlfriendsm.jpg", "girlfriend.html"]
+	]));
 
   document.querySelectorAll('.mdc-button').forEach(
     function(ele) {
